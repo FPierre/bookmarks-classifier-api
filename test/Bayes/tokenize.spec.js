e1d370651679fa07e054ac1dbb5f95420c3dee04
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-10-04 08:39:47 +0200

    specs methode tokenize

--- test/Bayes/tokenize.spec.js
+++ test/Bayes/tokenize.spec.js
@@ -8,9 +8,36 @@ test.beforeEach(t => {
 
 test('success to lower case text', t => {
   const { bayes } = t.context
-  const text = 'TEXT TO TOKENIZE'
+  const text = 'TEXT TO LOWER CASE'
 
   const tokenized = bayes.tokenize(text)
 
-  t.deepEqual(['text', 'to', 'tokenize'], tokenized)
+  t.deepEqual(['text', 'to', 'lower', 'case'], tokenized)
+})
+
+test('success to sanitize text', t => {
+  const { bayes } = t.context
+  const text = ' text | to &&sanitize'
+
+  const tokenized = bayes.tokenize(text)
+
+  t.deepEqual(['text', 'to', 'sanitize'], tokenized)
+})
+
+test('success to trim text', t => {
+  const { bayes } = t.context
+  const text = ' text to trim  '
+
+  const tokenized = bayes.tokenize(text)
+
+  t.deepEqual(['text', 'to', 'trim'], tokenized)
+})
+
+test('success to tokenize text', t => {
+  const { bayes } = t.context
+  const text = "  TEXT -to- t'okenize .  "
+
+  const tokenized = bayes.tokenize(text)
+
+  t.deepEqual(['text', 'to', 't', 'okenize'], tokenized)
 })
