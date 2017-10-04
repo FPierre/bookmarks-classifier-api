f437d4db30ed731d0ce5f9e940b65a33101703be
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-10-04 15:44:57 +0200

    Rectification des noms des tests

--- test/Bayes/textCount.spec.js
+++ test/Bayes/textCount.spec.js
@@ -6,7 +6,7 @@ test.beforeEach(t => {
   t.context.bayes = bayes
 })
 
-test('success to return train the model', t => {
+test('success to return 0 for non-existent tag', t => {
   const { bayes } = t.context
 
   const count = bayes.textCount('javascript')
@@ -14,7 +14,7 @@ test('success to return train the model', t => {
   t.deepEqual(count, 0)
 })
 
-test('success to trains the model', t => {
+test('success to return the sum of texts for existent tag', t => {
   const { bayes } = t.context
 
   bayes.train('Article about JavaScript', 'javascript')
